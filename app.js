d4b4c8d63f4dde1c284671909678d9163304c4e6
daanreede <93087304+Daan-Reede@users.noreply.github.com>
2023-11-19 19:46:03 +0100

    template kinda

---
+++ app.js
@@ -0,0 +1,47 @@
+const { CosmosClient } = require('@azure/cosmos');
+const express = require('express')
+const path = require('path')
+const DataDAO = require('./dataDAO')
+require('dotenv').config()
+
+// Replace these values with your own Azure Cosmos DB credentials and endpoint
+const endpoint = process.env.END_POINT;
+const key = process.env.KEY;
+const databaseId = process.env.DATABASE_ID;
+const containerId = process.env.CONTAINER_ID;
+
+const app = express()
+app.use(express.static(path.join(__dirname, 'public')))
+
+const cosmosClient = new CosmosClient({ endpoint, key });
+
+const dataDAO = new DataDAO(cosmosClient, databaseId, containerId)
+dataDAO.init();
+
+// async function connectToCosmosDB() {
+//   // Create or retrieve the database
+//   const { database } = await client.databases.createIfNotExists({ id: databaseId });
+//   console.log(`Connected to database: ${database.id}`);
+
+//   // Create or retrieve the container
+//   const { container } = await database.containers.createIfNotExists({ id: containerId });
+//   console.log(`Connected to container: ${container.id}`);
+
+//   return container;
+// }
+
+// connectToCosmosDB().catch((error) => {
+//   console.error('Error connecting to Cosmos DB:', error);
+// });
+
+
+app.listen(8080, () => {
+  console.log("Server successfully running on port 8080");
+});
+
+app.get('/api/getItems', async(req, res) => {
+  
+  const resources = await dataDAO.getAllItems();
+
+  return res.json(resources)
+});
---
+++ dataDAO.js
@@ -0,0 +1,39 @@
+class DataDAO {
+    constructor(cosmosClient, databaseId, containerId) {
+        this.client = cosmosClient
+        this.databaseId = databaseId
+        this.collectionId = containerId
+   
+        this.database = null
+        this.container = null
+      }
+   
+      async init() {
+        console.log('Setting up the database...')
+        const dbResponse = await this.client.databases.createIfNotExists({
+          id: this.databaseId
+        })
+        this.database = dbResponse.database
+        console.log('Setting up the database...done!')
+        console.log('Setting up the container...')
+        const coResponse = await this.database.containers.createIfNotExists({
+          id: this.collectionId
+        })
+        this.container = coResponse.container
+        console.log('Setting up the container...done!')
+      }
+
+      async getAllItems() {
+        const querySpec = {
+            query: 'SELECT TOP 2 * FROM c',
+            parameters: [],
+        };
+          
+        const { resources } = await this.container.items.query(querySpec).fetchAll();
+        console.log('Getting an item from the database')
+
+        return resources
+      }
+}
+
+module.exports = DataDAO;

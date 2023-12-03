c30780cf3ceb121825eebb8c0bedc2d453911f00
daanreede <93087304+Daan-Reede@users.noreply.github.com>
2023-12-03 11:52:55 +0100

    added comments

--- dataDAO.js
+++ dataDAO.js
@@ -1,40 +1,71 @@
+const { CosmosClient, Container, Database } = require("@azure/cosmos");
+
 /**
- * 
+ * Represents a Data Access Object (DAO) for interacting with a Cosmos DB database.
+ *
+ * @class DataDAO
+ * @constructor
+ * @param {CosmosClient} cosmosClient - The Cosmos DB client for connecting to the database.
+ * @param {string} databaseId - The ID of the Cosmos DB database.
+ * @param {string} containerId - The ID of the container within the database.
+ *
+ * @property {CosmosClient} client - The Cosmos DB client.
+ * @property {string} databaseId - The ID of the Cosmos DB database.
+ * @property {string} collectionId - The ID of the container within the database.
+ * @property {Database} database - Reference to the initialized Cosmos DB database.
+ * @property {Container} container - Reference to the initialized container within the database.
  */
 class DataDAO {
-    constructor(cosmosClient, databaseId, containerId) {
-        this.client = cosmosClient
-        this.databaseId = databaseId
-        this.collectionId = containerId
-   
-        this.database = null
-        this.container = null
-      }
-   
-      async init() {
-        const dbResponse = await this.client.databases.createIfNotExists({
-          id: this.databaseId
-        })
-        this.database = dbResponse.database
-        console.log('Setting up the database...done!')
+  constructor(cosmosClient, databaseId, containerId) {
+    this.client = cosmosClient;
+    this.databaseId = databaseId;
+    this.collectionId = containerId;
 
-        const coResponse = await this.database.containers.createIfNotExists({
-          id: this.collectionId
-        })
-        this.container = coResponse.container
-        console.log('Setting up the container...done!')
-      }
+    this.database = null;
+    this.container = null;
+  }
 
-      async getAllItems() {
-        const querySpec = {
-            query: 'SELECT TOP 50 * FROM c'
-        };
-          
-        const { resources } = await this.container.items.query(querySpec).fetchAll();
-        console.log('Getting an item from the database')
+  /**
+   * Initializes the Cosmos DB database and container. If they do not exist, it creates them.
+   *
+   * @async
+   * @method init
+   * @returns {Promise<void>}
+   */
 
-        return resources
-      }
+  async init() {
+    const dbResponse = await this.client.databases.createIfNotExists({
+      id: this.databaseId,
+    });
+    this.database = dbResponse.database;
+    console.log("Setting up the database...done!");
+
+    const coResponse = await this.database.containers.createIfNotExists({
+      id: this.collectionId,
+    });
+    this.container = coResponse.container;
+    console.log("Setting up the container...done!");
+  }
+
+  /**
+   * Retrieves the top 50 items from the container using a SQL query.
+   *
+   * @async
+   * @method getAllItems
+   * @returns {Promise<Array>} - A promise that resolves with an array of items from the container.
+   */
+  async getAllItems() {
+    const querySpec = {
+      query: "SELECT TOP 50 * FROM c",
+    };
+
+    const { resources } = await this.container.items
+      .query(querySpec)
+      .fetchAll();
+    console.log("Getting an item from the database");
+
+    return resources;
+  }
 }
 
 module.exports = DataDAO;
--- index.js
+++ index.js
@@ -4,18 +4,24 @@ const path = require('path')
 const DataDAO = require('./dataDAO');
 require('dotenv').config();
 
+// Retrieve configuration values from environment variables
 const port = process.env.PORT || 8080;
 const endpoint = process.env.END_POINT;
 const key = process.env.KEY;
 const databaseId = process.env.DATABASE_ID;
 const containerId = process.env.CONTAINER_ID;
 
+// Initialize Express application
 const app = express()
+
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Initialize Cosmos DB client and DataDAO
 const cosmosClient = new CosmosClient({ endpoint, key });
 const dataDAO = new DataDAO(cosmosClient, databaseId, containerId)
 
+// Initialize Cosmos DB database and container
 dataDAO.init()
   .catch(err => {
     console.error(err)
@@ -25,11 +31,21 @@ dataDAO.init()
     process.exit(1)
   });
 
+// Start Express server and listen on the specified port
 app.listen(port, () => {
   console.log("Server successfully running on port 8080, visit http://localhost:8080");
 });
 
+// Define API endpoint to get items
 app.get('/api/getItems', async (req, res) => {
-  const resources = await dataDAO.getAllItems();
-  return res.json(resources)
+  try {
+    // Retrieve items using DataDAO
+    const resources = await dataDAO.getAllItems();
+    // Return items as JSON response
+    return res.json(resources);
+  } catch (error) {
+    // Handle errors during item retrieval
+    console.error(error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
